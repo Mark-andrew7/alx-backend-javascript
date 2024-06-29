f22732d65efd842d938e965b4bcc3f7643a4eb96
Mark-andrew7 <kamaumark19@gmail.com>
2024-06-29 08:18:48 +0300

    Changed indentation to 2 spaces

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -1,47 +1,47 @@
-import Currency from './3-currency.js';
+import Currency from './3-currency';
 
 export default class Pricing {
-    constructor(amount, currency) {
-        this._amount = this.verifyType(amount, 'number', 'Amount');
-        this._currency = this.verifyCurrency(currency);
+  constructor(amount, currency) {
+    this._amount = this.verifyType(amount, 'number', 'Amount');
+    this._currency = this.verifyCurrency(currency);
+  }
+
+  get amount() {
+      return this._amount;
+  }
+
+  set amount(value) {
+    this._amount = this.verifyType(amount, 'number', 'Amount');
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(value) {
+    this._currency = this.verifyCurrency(currency);
+  }
+
+  verifyType(value, expectedType, attributeName) {
+    if (typeof value !== expectedType) {
+      throw new TypeError(`${attributeName} must be a ${expectedType}`)
     }
-
-        get amount() {
-            return this._amount;
+    return value;
         }
 
-        set amount(value) {
-            this._amount = this.verifyType(amount, 'number', 'Amount');
-        }
+  verifyCurrency(currency) {
+    if (!(currency instanceof Currency)) {
+      throw new TypeError('Currency must be an instance of Currency class')
+    }
+    return currency;
+  }
 
-        get currency() {
-            return this._currency;
-        }
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.name} (${this._currency.code})`
+  }
 
-        set currency(value) {
-            this._currency = this.verifyCurrency(currency);
-        }
-
-        verifyType(value, expectedType, attributeName) {
-            if (typeof value !== expectedType) {
-                throw new TypeError(`${attributeName} must be a ${expectedType}`)
-            }
-            return value;
-        }
-
-        verifyCurrency(currency) {
-            if (!(currency instanceof Currency)) {
-                throw new TypeError('Currency must be an instance of Currency class')
-            }
-            return currency;
-        }
-
-        displayFullPrice() {
-            return `${this._amount} ${this._currency.name} (${this._currency.code})`
-        }
-
-        static convertPrice(amount, conversionRate) {
-            return amount * conversionRate;
-        }
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
 
 }
