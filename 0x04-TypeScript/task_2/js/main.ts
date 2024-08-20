e3eec54a99e2c06881eaf0f4429dc8abe10b68ed
Mark-andrew7 <kamaumark19@gmail.com>
2024-08-20 11:53:27 +0300

    Executing basic javascript with Node JS

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -57,3 +57,15 @@ interface DirectorInterface {
       console.log(employee.workTeacherTasks());
     }
   }
+
+  type Subjects = 'Math' | 'History';
+
+function teachClass(todayClass: Subjects): string {
+  if (todayClass === 'Math') {
+    return 'Teaching Math';
+  } else if (todayClass === 'History') {
+    return 'Teaching History';
+  } else {
+    throw new Error('Invalid subject');
+  }
+}
