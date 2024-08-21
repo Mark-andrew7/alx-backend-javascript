52cdfad66ef2911d690a2660d0701c5a6d3cd25d
Mark-andrew7 <kamaumark19@gmail.com>
2024-08-21 07:54:52 +0300

    Fixed linter issues

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -2,8 +2,8 @@ const readline = require('readline');
 
 const r1 = readline.createInterface({
   input: process.stdin,
-  output: process.stdout
-})
+  output: process.stdout,
+});
 
 console.log('Welcome to Holberton School, what is your name?');
 
